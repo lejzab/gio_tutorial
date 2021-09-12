7247b1a3ace2a87a794796b49547af6dbff59e0a
Paweł Bogaczewicz <pawel.bogaczewicz@outlook.com>
2021-09-12 20:11:30 +0200

    button

--- go.mod
+++ go.mod
@@ -7,5 +7,8 @@ require gioui.org v0.0.0-20210912162729-fc5637804f32
 require (
 	gioui.org/cpu v0.0.0-20210817075930-8d6a761490d2 // indirect
 	gioui.org/shader v1.0.2 // indirect
+	golang.org/x/exp v0.0.0-20210722180016-6781d3edade3 // indirect
+	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/text v0.3.6 // indirect
 )
--- main.go
+++ main.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"gioui.org/app"
+	"gioui.org/font/gofont"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
 	"gioui.org/unit"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
 )
 
 func main() {
@@ -12,8 +18,28 @@ func main() {
 			app.Title("Egg timer"),
 			app.Size(unit.Dp(400), unit.Dp(600)))
 
+		// ops are the operations from the UI
+		var ops op.Ops
+
+		// startButton is a clickable widget
+		var startButton widget.Clickable
+
+		// th defnes the material design style
+		th := material.NewTheme(gofont.Collection())
+
 		// listen for events in the window.
-		for range w.Events() {
+		for e := range w.Events() {
+
+			// detect what type of event
+			switch e := e.(type) {
+
+			// this is sent when the application should re-render.
+			case system.FrameEvent:
+				gtx := layout.NewContext(&ops, e)
+				btn := material.Button(th, &startButton, "Start")
+				btn.Layout(gtx)
+				e.Frame(gtx.Ops)
+			}
 		}
 	}()
 	app.Main()

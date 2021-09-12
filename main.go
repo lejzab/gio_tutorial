097a47f7bdac363846c1880c2dee9fbfdb275ca4
Paweł Bogaczewicz <pawel.bogaczewicz@outlook.com>
2021-09-12 20:03:35 +0200

    set size and name

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"gioui.org/app"
+	"gioui.org/unit"
 )
 
 func main() {
 	go func() {
 		// create new window
-		w := app.NewWindow()
+		w := app.NewWindow(
+			app.Title("Egg timer"),
+			app.Size(unit.Dp(400), unit.Dp(600)))
 
 		// listen for events in the window.
 		for range w.Events() {

0e9ea65162163d0b773bba340ad14c6d0944c3ee
Paweł Bogaczewicz <pawel.bogaczewicz@outlook.com>
2021-09-12 20:29:43 +0200

    little rafactoring

--- main.go
+++ main.go
@@ -9,38 +9,50 @@ import (
 	"gioui.org/unit"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
+	"log"
+
+	"os"
 )
 
 func main() {
 	go func() {
-		// create new window
 		w := app.NewWindow(
 			app.Title("Egg timer"),
 			app.Size(unit.Dp(400), unit.Dp(600)))
-
-		// ops are the operations from the UI
-		var ops op.Ops
-
-		// startButton is a clickable widget
-		var startButton widget.Clickable
-
-		// th defnes the material design style
-		th := material.NewTheme(gofont.Collection())
-
-		// listen for events in the window.
-		for e := range w.Events() {
-
-			// detect what type of event
-			switch e := e.(type) {
-
-			// this is sent when the application should re-render.
-			case system.FrameEvent:
-				gtx := layout.NewContext(&ops, e)
-				btn := material.Button(th, &startButton, "Start")
-				btn.Layout(gtx)
-				e.Frame(gtx.Ops)
-			}
+		if err := loop(w); err != nil {
+			log.Fatal(err)
 		}
+		os.Exit(0)
 	}()
 	app.Main()
+}
+
+func loop(w *app.Window) error {
+	// create new window
+
+	// ops are the operations from the UI
+	var ops op.Ops
+
+	// startButton is a clickable widget
+	var startButton widget.Clickable
+
+	// th defnes the material design style
+	th := material.NewTheme(gofont.Collection())
+	// listen for events in the window.
+	for e := range w.Events() {
+
+		// detect what type of event
+		switch e := e.(type) {
+		case system.DestroyEvent:
+			return e.Err
+		// this is sent when the application should re-render.
+		case system.FrameEvent:
+			gtx := layout.NewContext(&ops, e)
+			btn := material.Button(th, &startButton, "Start")
+			btn.Layout(gtx)
+			e.Frame(gtx.Ops)
+
+		}
+	}
+	return nil
 }
